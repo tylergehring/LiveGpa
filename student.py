0454280a15677a75718d000911a03288d0266706
Tyler Gehring <tyler.d.gehring@gmail.com>
2025-02-22 17:31:05 -0800

    program can now calculate student gpa... assuming 3 credit classes

--- student.py
+++ student.py
@@ -23,15 +23,17 @@ class Student():
             'last_name' : "None",
             'id' : 0,
             'courses' : [], #list of dicts {'class_name': class, 'class_id': id, 'grade' : grade}
-            'grades' : []
+            'gpa' : 0
         })
         self.year = None
         self.month = None
-
+        
+        ### ---- Filling Data ----
         self.get_date()
         self.get_name_and_id()
         self.get_courses()
         self.get_grades()
+        self.calc_gpa()
 
     def get_date(self):
         current_date = datetime.datetime.now()
@@ -40,8 +42,7 @@ class Student():
         
     def get_name_and_id(self):
         try:
-            headers = {'Authorization' : 'Bearer ' + self.apiKey}
-            rec = requests.get((f"{self.url}/users/self"), headers = headers) 
+            rec = requests.get((f"{self.url}/users/self"), headers = self.headers) 
             rec = rec.json()
             rec = DotDict(rec)
             self.data.first_name = rec.first_name
@@ -49,14 +50,13 @@ class Student():
             self.data.id = rec.id
         except Exception as e:
             self.errors.append(e)
-            print("ERROR::Student:get_name_and_id")
+            print("ERROR::Student:get_name_and_id... Probably a Key Issue")
             raise(e)
             
     
     def get_courses(self):
         rec = requests.get((f"{self.url}/courses/?per_page=30"), headers = self.headers) #request to student courses "?per_page=30" takes 30 results. the default is 10 due to the http pagination
         rec = rec.json()
-
         for course in rec:
             try:
                 #if the end date of the course is in the future, you are currently taking the class
@@ -64,7 +64,6 @@ class Student():
                 month = int(course['end_at'][5:7])
                 if ((year >= self.year) and (month >= self.month)):
                     self.data.courses.append({'name': course['name'], 'id': course['id'], 'grade' : None})
-
             except: # some entries wont be actual classes so we skip over them using a try/except block
                 pass
 
@@ -75,6 +74,31 @@ class Student():
             rec = requests.get((f"{self.url}/courses/{course['id']}/enrollments/?user_id={self.data.id}"), headers = self.headers) 
             rec = rec.json()
             course['grade'] = {'current_score': rec[0]['grades']['current_score'], 'current_grade': rec[0]['grades']['current_grade']}
-            
+
+
+    def calc_gpa(self):
+        """Calculates overall gpa, assumes all classes are three credits"""
+        assumed_credits = 3
+        total_credits = 0
+        total_score = 0
+        for course in self.data.courses:
+            if course['grade']['current_grade'] == 'A':
+                total_score += (4 * assumed_credits)
+            elif course['grade']['current_grade'] == 'B':
+                total_score += (3 * assumed_credits)
+            elif course['grade']['current_grade'] == 'C':
+                total_score += (2 * assumed_credits)
+            elif course['grade']['current_grade'] == 'D':
+                total_score += (1 * assumed_credits)
+            elif course['grade']['current_grade'] == 'F':
+                pass
+            elif course['grade']['current_grade'] == None:
+                pass
+            else:
+                self.errors.append(f"Unknown Grade for {self.data.first_name} {self.data.last_name}, course {course['name']}")
+            total_credits += 3
+
+        self.data.gpa = (total_score/total_credits)
+
 
         
